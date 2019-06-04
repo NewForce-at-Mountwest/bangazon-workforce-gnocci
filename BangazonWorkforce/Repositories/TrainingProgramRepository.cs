ea5a7ee852cb09906c1abd8b3f8295885286de60
Tommy Spurlock <tspurloc@gmail.com>
2019-06-04 13:38:47 -0400

    removed uncommented code. Ready for Merge.

--- BangazonWorkforce/Repositories/TrainingProgramRepository.cs
+++ BangazonWorkforce/Repositories/TrainingProgramRepository.cs
@@ -68,49 +68,7 @@ namespace BangazonWorkforce.Repositories
 
             }
 
-        //public static TrainingProgram GetOneTrainingProgram(int id)
-        //{
-        //    using (SqlConnection conn = Connection)
-        //    {
-        //        conn.Open();
-        //        using (SqlCommand cmd = conn.CreateCommand())
-        //        {
-        //            cmd.CommandText = @"
-        //                SELECT
-        //                    Id, Name, StartDate, EndDate, MaxAttendees
-        //                FROM TrainingProgram
-        //                WHERE Id = @id";
-        //            cmd.Parameters.Add(new SqlParameter("@id", id));
-        //            SqlDataReader reader = cmd.ExecuteReader();
 
-        //            TrainingProgram TrainingProgram = null;
-
-        //            if (reader.Read())
-        //            {
-        //                TrainingProgram = new TrainingProgram
-        //                {
-        //                    Id = reader.GetInt32(reader.GetOrdinal("Id")),
-        //                    Name = reader.GetString(reader.GetOrdinal("firstName")),
-        //                    StartDate = reader.GetDateTime(reader.GetOrdinal("StartDate")),
-        //                    EndDate = reader.GetDateTime(reader.GetOrdinal("EndDate")),
-        //                    MaxAttendees = reader.GetInt32(reader.GetOrdinal("MaxAttendees")),
-
-        //                };
-        //            }
-        //            reader.Close();
-
-        //            return TrainingProgram;
-        //        }
-        //    }
-
-        //}
-
-        //public static Student GetOneStudentWithExercises(int id)
-        //{
-        //    Student student = GetOneStudent(id);
-        //    student.Exercises = ExerciseRepository.GetAssignedExercisesByStudent(id);
-        //    return student;
-        //}
 
         public static void CreateTrainingProgram(TrainingProgram trainingProgram)
         {
@@ -135,66 +93,6 @@ namespace BangazonWorkforce.Repositories
 
         }
 
-        //public static void UpdateStudent(int id, EditStudentViewModel viewModel)
-        //{
-        //    using (SqlConnection conn = Connection)
-        //    {
-        //        conn.Open();
-        //        using (SqlCommand cmd = conn.CreateCommand())
-        //        {
-        //            string command = @"UPDATE Student
-        //                                SET firstName=@firstName, 
-        //                                lastName=@lastName, 
-        //                                slackHandle=@slackHandle, 
-        //                                cohortId=@cohortId
-        //                                WHERE Id = @id
-        //                                DELETE FROM StudentExercise WHERE studentId =@id";
-
-        //            viewModel.SelectedExercises.ForEach(exerciseId =>
-        //            {
-        //                command += $" INSERT INTO StudentExercise (studentId, exerciseId) VALUES (@id, {exerciseId})";
-
-        //            });
-        //            cmd.CommandText = command;
-        //            cmd.Parameters.Add(new SqlParameter("@firstName", viewModel.student.FirstName));
-        //            cmd.Parameters.Add(new SqlParameter("@lastName", viewModel.student.LastName));
-        //            cmd.Parameters.Add(new SqlParameter("@slackHandle", viewModel.student.SlackHandle));
-        //            cmd.Parameters.Add(new SqlParameter("@cohortId", viewModel.student.CohortId));
-        //            cmd.Parameters.Add(new SqlParameter("@id", id));
-
-        //            int rowsAffected = cmd.ExecuteNonQuery();
-
-        //        }
-
-        //    }
-
-        //}
-
-        //public static void DeleteStudent(int id)
-        //{
-        //    using (SqlConnection conn = Connection)
-        //    {
-        //        conn.Open();
-        //        using (SqlCommand cmd = conn.CreateCommand())
-        //        {
-        //            cmd.CommandText = @"DELETE FROM StudentExercise WHERE studentId = @id";
-        //            cmd.Parameters.Add(new SqlParameter("@id", id));
-
-        //            int rowsAffected = cmd.ExecuteNonQuery();
-
-        //        }
-        //        using (SqlCommand cmd = conn.CreateCommand())
-        //        {
-        //            cmd.CommandText = @"DELETE FROM Student WHERE Id = @id";
-        //            cmd.Parameters.Add(new SqlParameter("@id", id));
-
-        //            int rowsAffected = cmd.ExecuteNonQuery();
-
-        //        }
-
-        //    }
-
-        //}
 
     }
 }
